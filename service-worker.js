7f26e5d89ed2d0e4d83bf4e4704dfeca63e2f413
jangsus1 <min99101511@naver.com>
2020-01-03 20:32:40 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee11a0de2df76dadc26b08ee66441dc8.js"
+  "/precache-manifest.0f3ea997a037d26e87422bfbfdec2a3d.js"
 );
 
 self.addEventListener('message', (event) => {
